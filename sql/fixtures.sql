94dbd9549a3fe3103d8940c840e7b7bb804e40e7
Geoffrey Abbott <geoff@metacore.tech>
2018-11-26 16:54:31 -0500

    dropped email from user fields

--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -4,6 +4,6 @@
 
 -- msgr_user ----------------------------------------------------------------------------
 INSERT INTO msgr.msgr_user 
-		(Email, Password, UserName) 
+		(UserName, Password) 
 	VALUES 
-		('geoff@test-dev.com', '$2a$10$8283VgLJDQJ7.L/8/kHGJeMoeNxehft5HE1fPv7q3pVRodn6TihsK', 'gabbott');
+		('gabbott', '$2a$10$8283VgLJDQJ7.L/8/kHGJeMoeNxehft5HE1fPv7q3pVRodn6TihsK');
--- sql/init_db.sql
+++ sql/init_db.sql
@@ -9,14 +9,12 @@ CREATE SCHEMA IF NOT EXISTS msgr;
 -- The main msgr user table
 CREATE TABLE IF NOT EXISTS msgr.msgr_user (
     MsgrUserId              SERIAL       NOT NULL,
-    Email                   VARCHAR(100) NOT NULL CHECK (char_length(Email)  >= 3)      CONSTRAINT "DF_MsgrUser_Email"      DEFAULT(null),
-    Password                VARCHAR(100) NOT NULL CHECK (char_length(Password)  >= 8)   CONSTRAINT "DF_MsgrUser_Password"   DEFAULT(null),
     UserName                VARCHAR(100) NOT NULL CHECK (char_length(UserName)  >= 3)   CONSTRAINT "DF_MsgrUser_UserName"   DEFAULT(null),
+    Password                VARCHAR(100) NOT NULL CHECK (char_length(Password)  >= 8)   CONSTRAINT "DF_MsgrUser_Password"   DEFAULT(null),
     UserActiveStatus        BOOLEAN      NOT NULL CONSTRAINT "DF_MsgrUser_UserStatus"   DEFAULT (TRUE),
     CreateDate              TIMESTAMP    NOT NULL CONSTRAINT "DF_MsgrUser_CreateDate"   DEFAULT (NOW()),
     ModifiedDate            TIMESTAMP    NOT NULL CONSTRAINT "DF_MsgrUser_ModifiedDate" DEFAULT (NOW()),
     CONSTRAINT "PK_MsgrUser_MsgrUserId" PRIMARY KEY (MsgrUserId),
-    UNIQUE(Email),
     UNIQUE(UserName)
 );
 
